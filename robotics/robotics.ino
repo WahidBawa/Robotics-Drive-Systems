3d65069a7408a39a7ff424640bfde426b66cff5d
Wahid Bawa <wahidbawa10@gmail.com>
2018-10-05 05:48:27 -0400

    Commented Code

--- robotics/robotics.ino
+++ robotics/robotics.ino
@@ -1,30 +1,27 @@
-#include <Servo.h>
-int rightAdd = 0;
-int leftAdd = 0;
-Servo leftServo;
-Servo rightServo;
+#include <Servo.h> // This imports the header file for servo functions
+int leftAdd, rightAdd; // intializes these variables
+Servo leftServo, rightServo; // Creates and initializes the servo objects
 void setup() {
-  rightServo.attach(A4);
-  leftServo.attach(A3);
-  rightServo.write(90);
-  leftServo.write(90);
-  Serial.begin(9600);
+  rightServo.attach(A4); // rightServo inputs/outputs will come from the specified port
+  leftServo.attach(A3); // leftServo inputs/outputs will come from the specified port
+  rightServo.write(90); // Writes the stop states to the 360 servos
+  leftServo.write(90); // Writes the stop states to the 360 servos
 }
 
 void loop() {
-  int joyX = analogRead(A1);
-  int joyY = analogRead(A2);
-  int mapY = map(joyY, 0, 1023, 80, 100);
-  int mapX = map(joyX, 0, 1023, 80, 100);
+  int joyX = analogRead(A1); // Reads input from the joystick
+  int joyY = analogRead(A2); // Reads the input from the joystick
+  int mapY = map(joyY, 0, 1023, 0, 180); // Converts the inputs from the joystick inputs to be written to the servos
+  int mapX = map(joyX, 0, 1023, 0, 180); // Converts the inputs from the joystick inputs to be written to the servos
 //  int mapY = joyY << 3; // This is bit shifting from McKenzie, Should try it out to be more efficient
 //  int mapX = joyX << 3;
   leftAdd = mapX - 90; // This will take the mapped value minus the center point of the 360 servos
-  rightAdd = mapX - 90;
+  rightAdd = mapX - 90; // This will take the mapped value minus the center point of the 360 servos
 
-  int rightServoVal = mapY + rightAdd;
-  int leftServoVal = 90 + 90 - mapY + leftAdd;
+  int rightServoVal = mapY + rightAdd; // This will set the outputs for the servo
+  int leftServoVal = 180 - mapY + leftAdd; // This will set the outputs of the servo
   
-  rightServo.write(rightServoVal);
-  leftServo.write(leftServoVal);
+  rightServo.write(rightServoVal); // Writes the values to the servo
+  leftServo.write(leftServoVal); // Writes the values to the servo
 
 }
